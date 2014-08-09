83dd76b00a9f1c88e30b2494f79b415e9dba0264
SWBrasil <jaime.tsuchiya@gmail.com>
2014-08-09 13:07:23 -0300

    Templates

---
+++ Templates/themeforest-3091853-reverence-church-responsive-html-5-theme/Reverence/html/contact/thank-you.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"  "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en-US" lang="en-US">
+<head>
+<meta http-equiv='Content-Type' content='text/html; charset=utf-8'/>
+<title>Thank you!</title>
+<style type="text/css">@import url("http://fonts.googleapis.com/css?family=Yanone+Kaffeesatz");</style>
+
+<style type="text/css">
+
+h3 {
+	color:#000000;
+	font-size:22px;
+	font-weight:normal;
+	font-family: 'Yanone Kaffeesatz', sans-serif;
+	margin-top:12px;
+	margin-left:0px;
+	background-color:#ffffff;
+}
+
+</style>
+
+</head>
+
+<body>
+
+<h3>THANKS FOR CONTACTING US!</h3>
+
+</body>
+</html>
